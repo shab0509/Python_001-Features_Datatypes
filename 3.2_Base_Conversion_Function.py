dfb7573f5229d7a3a97e723ea542a45edcec9f7a
Shabi Victor <38999931+shab0509@users.noreply.github.com>
2023-07-07 13:53:34 +0530

    Add files via upload

---
+++ 3.2_Base_Conversion_Function.py
@@ -0,0 +1,17 @@
+# Base Conversion Functions
+# Purpose of base conversion function is to convert one number system to another number system.
+# Decimal/Binary/Octal/Hex
+
+a=0XA2231
+print(a,type(a))
+
+b = bin(a)
+print(b)
+
+c= oct(a)
+print(c)
+
+d= hex(a)
+print(d)
+
+
